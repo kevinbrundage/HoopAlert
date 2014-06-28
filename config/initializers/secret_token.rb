6c130a5d7de9da228cebe386ce923118fd9fa330
Kevin Brundage <kbrundage@futuresinc.com>
2014-06-28 15:50:51 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoopAlert::Application.config.secret_token = '6859a502a8c4ebe62a82b90c1cc5a6f269f8eee178526cf437c70865bbca838c618e0f90cf220dedbf6b7cf8f469659a1dbe6fe276a94cc702b19cb76da27614'
