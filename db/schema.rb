4323ca99aabd17142ae1090f4e141aec7961fd29
chopsmith <sbrundage@Stevens-MacBook-Pro.local>
2014-07-20 17:12:33 -0400

    Remove hashed_password from migration
    Getting rid of that hashed password because it will be taken care of by
    rails has_secure_password password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,9 @@ ActiveRecord::Schema.define(:version => 20140629190757) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "hashed_password"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
